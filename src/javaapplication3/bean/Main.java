8d49e1e96f43de34d9cc065e0ef7c477f0ab2640
Henrique Sciani <135352184+HenriqueSciani@users.noreply.github.com>
2024-06-17 19:13:15 -0300

    calculadora

---
+++ src/javaapplication3/bean/Main.java
@@ -0,0 +1,11 @@
+package javaapplication3.bean;
+
+import controller.AlunoController;
+
+
+public class Main {
+    public static void main(String[] args) {
+        AlunoController alunocontroller = new AlunoController();
+        alunocontroller.calc();
+    }
+}
